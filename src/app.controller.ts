10a33c66c54f2897c57e00c263f13363312f13ef
HyunJinNo <nhj9831@naver.com>
2024-07-08 11:46:38 +0900

    @nestjs/config 모듈 사용하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller() // 컨트롤러 데코레이터
 export class AppController {
   // 외부에서 사용하므로 export를 붙임.
 
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
-  @Get() // GET 요청 처리 데코레이턴
+  @Get() // GET 요청 처리 데코레이터
   getHello(): string {
-    return this.appService.getHello();
+    const message = this.configService.get('MESSAGE'); // configService.get() 호출
+    return `${message}. ${this.appService.getHello()}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BlogModule } from './blog/blog.module';
+import { BlogModule } from './modules/blog/blog.module';
+import { ConfigModule } from '@nestjs/config';
 
 // 모듈 데코레이터
 @Module({
-  imports: [BlogModule],
+  // ConfigModule 설정
+  // 전역 모듈 설정 추가
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
