1edee15474d44dd1ba7fa36b76fe3eb4ef7cb1a5
HyunJinNo <nhj9831@naver.com>
2024-08-11 10:08:01 +0900

    Feat: LocalStrategy 생성

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AuthController],
+  imports: [UserModule, PassportModule.register({ session: true })],
+  controllers: [AuthController, LocalStrategy, SessionSerializer],
   providers: [AuthService],
 })
 export class AuthModule {}
