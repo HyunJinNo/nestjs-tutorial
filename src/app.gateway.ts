e333767c61c0ef98b5f8fa77cdc4b0218476be3a
HyunJinNo <nhj9831@naver.com>
2024-08-15 17:14:15 +0900

    Feat: 채팅방 입장 기능

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -26,6 +26,8 @@ export class ChatGateway {
 
 @WebSocketGateway({ namespace: 'room' }) // room 네임스페이스를 사용하는 게이트웨이
 export class RoomGateway {
+  // 채팅 게이트웨이 의존성 주입
+  constructor(private readonly chatGateway: ChatGateway) {}
   rooms = [];
 
   // 서버 인스턴스 접근을 위한 변수 선언
@@ -34,9 +36,24 @@ export class RoomGateway {
   @SubscribeMessage('createRoom') // createRoom 핸들러 메서드
   handleMessage(@MessageBody() data: any) {
     // 소켓없이 데이터만 받음.
-
     const { room, nickname } = data;
+
+    // 방 생성 시 이벤트를 발생시켜 클라이언트에 송신
+    this.chatGateway.server.emit('notice', {
+      message: `${nickname}님이 ${room}방을 만들었습니다.`,
+    });
+
     this.rooms.push(room); // 채팅방 정보를 받아서 추가
     this.server.emit('rooms', this.rooms); // rooms 이벤트로 채팅방 리스트 전송
+  }
+
+  @SubscribeMessage('joinRoom') // 방 입장 시 실행되는 핸들러 메서드
+  handleJoinRoom(@ConnectedSocket() socket: Socket, @MessageBody() data: any) {
+    const { room, nickname, toLeaveRoom } = data;
+    socket.leave(toLeaveRoom); // 기존의 방에서 먼저 나간다.
+    this.chatGateway.server.emit('notice', {
+      message: `${nickname}님이 ${room}방에 입장했습니다.`,
+    });
+    socket.join(room); // 새로운 방에 입장합니다.
   }
 }
--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@
 const socket = io('http://localhost:3000/chat'); // 네임스페이스 추가
 const roomSocket = io('http://localhost:3000/room'); // 채팅방용 네임스페이스 생성
 const nickname = prompt('닉네임을 입력해 주세요.'); // 닉네임 입력받기
+let currentRoom = ''; // 채팅방 초깃값
 
 // [전송] 버튼 클릭 시 입력된 글을 message 이벤트로 보냄
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -18,6 +19,14 @@ function createRoom() {
   roomSocket.emit('createRoom', { room, nickname });
 }
 
+// 방에 들어갈 때 기존에 있던 방에서는 나가기
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function joinRoom(room) {
+  // 서버 측의 joinRoom 이벤트를 발생시킴
+  roomSocket.emit('joinRoom', { room, nickname, toLeaveRoom: currentRoom });
+  currentRoom = room; // 현재 들어 있는 방의 값을 변경
+}
+
 // 클라이언트 측에서 채팅방을 추가하는 함수
 roomSocket.on('rooms', (data) => {
   console.log(data);
@@ -30,11 +39,16 @@ roomSocket.on('rooms', (data) => {
 });
 
 // 서버 접속을 확인을 위한 이벤트
-socket.on('content', () => {
+socket.on('connect', () => {
   console.log('connected');
 });
 
 // 서버에서 message 이벤트 발생 시 처리
 socket.on('message', (message) => {
   $('#chat').append(`<div>${message}</div>`);
+});
+
+// notice 이벤트를 받아서 처리
+socket.on('notice', (data) => {
+  $('#notice').append(`<div>${data.message}</div>`);
 });
