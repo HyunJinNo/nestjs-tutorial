fb7d3337b80e61fbd3915cd7e4a2383360cffc35
HyunJinNo <nhj9831@naver.com>
2024-10-29 20:16:55 +0900

    [refactor] 변수명 수정

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,12 +6,12 @@ import * as bcrypt from 'bcrypt';
 @Injectable() // 프로바이더로 사용
 export class AuthService {
   // 생성자에서 UserService를 주입받음.
-  constructor(private readonly userSerice: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   // 메서드 내부에서 await 구문이 있으므로 async 필요함.
   async register(userDto: CreateUserDto) {
     // 이미 가입된 유저가 있는지 체크
-    const user = await this.userSerice.getUser(userDto.email);
+    const user = await this.userService.getUser(userDto.email);
     if (user) {
       // 이미 가입된 유저가 있다면 에러 발생
       throw new HttpException(
@@ -26,7 +26,7 @@ export class AuthService {
     // 데이터베이스에 저장.
     // 저장 중 에러가 나면 서버 에러 발생
     try {
-      const user = await this.userSerice.createUser({
+      const user = await this.userService.createUser({
         ...userDto,
         password: encryptedPassword,
       });
@@ -41,8 +41,7 @@ export class AuthService {
 
   async validateUser(email: string, password: string) {
     // 이메일로 유저 정보를 가져옵니다.
-    const user = await this.userSerice.getUser(email);
-
+    const user = await this.userService.getUser(email);
     // 유저가 없으면 검증 실패
     if (!user) {
       return null;
